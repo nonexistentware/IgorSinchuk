5c3b7756c2571ca8181ddb27e033ecdb20a026bb
Nonexistent <nonexistentware@gmail.com>
2022-11-13 18:58:59 +0200

    Java basic homework #8

---
+++ src/ua/hillel/homework8/Main.java
@@ -0,0 +1,17 @@
+package ua.hillel.homework8;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(EnumColor.INFO);
+        System.out.println("Info");
+
+        System.out.println(EnumColor.DEBUG);
+        System.out.println("Debug");
+
+        System.out.println(EnumColor.WARN);
+        System.out.println("Warning");
+
+        System.out.println(EnumColor.ERROR);
+        System.out.println("Error");
+    }
+}
