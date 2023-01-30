39e3f23e022017fca6cf8131781d8c4cd6165eda
nonexistentware <59263938+nonexistentware@users.noreply.github.com>
2023-01-30 15:39:47 +0200

    Update StoreRunnerTest.java

--- src/test/java/ua/hillel/tests/RestTestJen/StoreRunnerTest.java
+++ src/test/java/ua/hillel/tests/RestTestJen/StoreRunnerTest.java
@@ -2,7 +2,7 @@ package ua.hillel.tests.RestTestJen;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ua.hillel.tests.RestTestJen.model.Store;
+import ua.hillel.RestTestJen.model.Store;
 
 import java.io.IOException;
 
