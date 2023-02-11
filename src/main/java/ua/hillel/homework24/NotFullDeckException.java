a5c8011efd6b077bd829039f7237200207c2cd6c
Nonexistent <nonexistentware@gmail.com>
2023-02-11 16:05:43 +0200

    Java basic homework 24

---
+++ src/main/java/ua/hillel/homework24/NotFullDeckException.java
@@ -0,0 +1,7 @@
+package ua.hillel.homework24;
+
+public class NotFullDeckException extends Exception{
+    public NotFullDeckException(String nfd) {
+        super(nfd);
+    }
+}
---
+++ src/main/java/ua/hillel/homework24/Poker.java
@@ -0,0 +1,11 @@
+package ua.hillel.homework24;
+
+public class Poker {
+    public  static  void main(String[] args) throws NotFullDeckException, JokerException, DeckNotShuffleException, NotEnoughPlayerException {
+        PokerMachine machine = new PokerMachine();
+        Deck deck = machine.createDeck();
+        machine.shuffleDeck(deck);
+
+        machine.serveCardsToPlayer(3, deck);
+    }
+}
